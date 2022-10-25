80cc43414fc0aaad98b80339089ee01a2c029ec4
Oroderu44 <Denys.tereshchenko44@gmail.com>
2022-10-25 04:18:38 -0400

    Nav
    in the works

--- js/main.js
+++ js/main.js
@@ -3,3 +3,34 @@ window.addEventListener('mousemove', e => {
     document.body.style.cssText += `--mousePositionX:${e.clientX/100}px; --mousePositionY:${e.clientY/100}px; `
 
 })
+
+/* Nav */
+
+
+var x = document.getElementById("Links1");
+var l = document.getElementById("lang");
+
+function myFunction() {
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else if(l.style.display = "block"){
+	l.style.display = "none";
+    x.style.display = "block";
+  }
+  else{
+  x.style.display = "block";
+  }
+};
+
+  
+function myFunction2() {
+  if (l.style.display === "block") {
+    l.style.display = "none";
+  } else if(x.style.display = "block"){
+	x.style.display = "none";
+    l.style.display = "block";
+  }
+  else{
+  l.style.display = "block";
+  }
+}
